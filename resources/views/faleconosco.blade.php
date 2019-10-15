d1c38b1a93b7436570f1cb7879f5fa55b44d4c46
Thiago <thiago.silva@sagatechbrasil.com.br>
2019-10-15 16:24:30 -0300

    botão whatsapp

--- resources/views/faleconosco.blade.php
+++ resources/views/faleconosco.blade.php
@@ -1,1 +1,9 @@
 @include('layout')
+<section>
+    <a href="http://link.ds7.com.br/506bfab" target="_blank">
+        <button type="button" class="btn btn-success" id="btn_whatsapp">
+          Fale com a Teofania pelo Whatsapp </button>
+    </a>
+
+</section>
+
