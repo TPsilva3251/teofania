9ec54f12e4051fe291f2feb5479f92f0f1d6d353
Thiago <thiago.silva@sagatechbrasil.com.br>
2019-10-15 16:25:11 -0300

    referenciando bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="/icone.jpg" type="image/x-icon" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <title>Teofania</title>
 </head>
 
@@ -51,7 +53,6 @@
         }
 
         #menu ul li a:hover {
-            background-color: #D6D6D6;
             color: #6D6D6D;
             border-bottom: 3px solid #EA0000;
         }
