1a73a790c9e7350ffb0f3cb1e3cff23045db8fea
Thiago <thiago.silva@sagatechbrasil.com.br>
2019-10-22 15:40:21 -0300

    modal teste

--- resources/views/biblias.blade.php
+++ resources/views/biblias.blade.php
@@ -1,1 +1,34 @@
 @include('layout')
+@include('biblias.1')
+<br><br>
+{{-- <div class="col-sm-3">
+        <img src="https://www.sbb.com.br/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/7/8/7899938412367_ra085tiber_baixa_2.jpg"
+        alt="" >
+        <button type="button" class="btn btn-outline-warning" data-toggle="modal" data-target="#biblia_1">
+            Ver detalhes
+        </button>
+</div> --}}
+<div class="thumbnail-wrapper col-sm-3">
+    <img src="https://www.sbb.com.br/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/7/8/7899938412367_ra085tiber_baixa_2.jpg" />
+    <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#biblia_1">
+        Ver detalhes
+    </button>
+</div>
+<style>
+    /* .btn-outline-warning{
+        margin-left: 15%;
+    } */
+.thumbnail-wrapper {
+    display:inline-block;
+    position:relative;
+}
+.thumbnail-wrapper button {
+    position:absolute;
+    top:50%;
+    left:37%;
+    -webkit-transform:translate3d(-50%, -50%, 0);
+    -moz-transform:translate3d(-50%, -50%, 0);
+    transform:translate3d(-50%, -50%, 0);
+}
+
+</style>
---
+++ resources/views/biblias/1.blade.php
@@ -0,0 +1,46 @@
+<div class="modal fade bd-example-modal-xl" id="biblia_1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">BÍBLIA DE ESTUDO DA REFORMA - EDIÇÃO LIMITADA</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="alignleft">
+            <img src="https://www.sbb.com.br/media/catalog/product/cache/1/small_image/210x/9df78eab33525d08d6e5fb8d27136e95/7/8/7899938412367_ra085tiber_baixa_2.jpg" />
+        </div>
+        <br><br>
+        <p><strong>A Bíblia de Estudo da Reforma apresenta um rico e vasto material como notas, artigos e orações com base em cada livro das Escrituras Sagradas. Todos os livros da Bíblia trazem uma introdução sobre o pensamento de Martinho Lutero, bem como um amplo esboço com detalhes históricos e artigos.</strong></p>
+        <br><br><br><br>
+        <div class="text">
+                <p><strong>TRADUÇÃO: </strong>Almeida Revista e Atualizada</p>
+                <p><strong>IDIOMA: </strong>Protuguês</p>
+                <p><strong>TAMANHO DA LETRA: </strong>Normal</p>
+                <p><strong>CAPA: </strong>Marrom</p>
+                <p><strong>ACABAMENTO: </strong>Couro sintético</p>
+                <p><strong>MEDIDA: </strong>16,2 x 23,4 cm</p>
+                <p><strong>FORMATO: </strong>Grande</p>
+                <p><strong>PÁGINAS: </strong>2.304 páginas</p>
+                <p><strong>ÍNDICE: </strong>Sim</p>
+                <p><strong>INDICADO PARA: </strong>Estudo da Reforma Protestante</p>
+                <p><strong>ISBN: </strong>7899938412367</p>
+                <p><strong>CÓDIGO: </strong>T2019EO0001</p>
+                <p><strong>PESO: </strong>1.4870Kg</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+<style>
+h5{
+  margin-left: 30%;
+   color: chocolate;
+}
+p{
+    font-size: 1em;
+}
+.alignleft { float: left; }
+
+</style>
