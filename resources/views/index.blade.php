6b956f1f1e38e88caf604147b7b2f6c93fa36508
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-01-19 03:58:20 -0300

    layout carrousel responsivo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,36 +35,38 @@
     </section> --}}
 
     <div class="content">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+           <div class="col">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
-                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                          <img src="/slide1.png" class="img-fluid d-block">
+                        <img src="/slide1.png" class="img-fluid d-block">
                         </div>
                         <div class="carousel-item">
-                          <img src="/slide2.png" class="img-fluid d-block">
+                        <img src="/slide2.png" class="img-fluid d-block">
                         </div>
                         <div class="carousel-item">
-                          <img src="/slide3.png" class="img-fluid d-block">
+                        <img src="/slide3.png" class="img-fluid d-block">
                         </div>
                         <div class="carousel-item">
                             <img src="/slide4.png" class="img-fluid d-block">
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Previous</span>
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
                     </a>
                     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Next</span>
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
                     </a>
                 </div>
+            </div>
     </div>
 
 </section>
